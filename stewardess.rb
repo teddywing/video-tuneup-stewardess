fa4e3c5caa13a96a04ed08b252de41d0697c45c2
Teddy Wing <ted@teddywing.com>
2012-04-25 17:37:52 -0400

    Put JSON output into a hash.

--- stewardess.rb
+++ stewardess.rb
@@ -10,12 +10,15 @@ require 'json'
 
 
 post '/blend/:filename' do
+  output = Hash.new
+  
   filename = File.join('up', params[:filename])
   datafile = params[:data]
   File.open(filename, 'wb') do |file|
     file.write(datafile[:tempfile].read)
   end
+  output['wrote'] = true
   
   content_type :json
-  { :wrote => true }.to_json
+  output.to_json
 end
