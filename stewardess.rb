2621f2b088e5897aa1999158a61b83058b5a8755
Teddy Wing <ted@teddywing.com>
2012-04-25 17:48:25 -0400

    Removed JSON output. Now outputting a file to stream (see http://sinatra.rubyforge.org/api/classes/Sinatra/Streaming.html)

--- stewardess.rb
+++ stewardess.rb
@@ -10,15 +10,11 @@ require 'json'
 
 
 post '/blend/:filename' do
-  output = Hash.new
-  
   filename = File.join('up', params[:filename])
   datafile = params[:data]
   File.open(filename, 'wb') do |file|
     file.write(datafile[:tempfile].read)
   end
-  output['wrote'] = true
   
-  content_type :json
-  output.to_json
+  send_file('up/thefile.jpg')
 end
