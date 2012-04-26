740c71c76d0a87e6903c0ab415a4f9b962ba89fc
Teddy Wing <ted@teddywing.com>
2012-04-26 02:17:07 -0400

    stewardess.rb: Integrated Brian's approach

--- stewardess.rb
+++ stewardess.rb
@@ -6,8 +6,6 @@
 require 'rubygems'
 require 'sinatra'
 
-require 'json'
-
 
 post '/blend/:filename' do
   filename = File.join('up', params[:filename])
@@ -15,12 +13,13 @@ post '/blend/:filename' do
   File.open(filename, 'wb') do |file|
     file.write(datafile[:tempfile].read)
   end
-
-  # Run something like:
-  # length = params[:length] # Desired length (in seconds)
-  # [console] python src/earworm.py -d #{length} -l #{filename}
-  # send_file("*.mp3")
-  # [console] rm *.mp3
   
-  send_file('up/thefile.jpg')
+  # Run something like:
+  length = params[:length] # Desired length (in seconds)
+  `python src/earworm.py -d #{length} -l #{filename}`
+  # send_file("*.mp3")
+  
+  send_file("up/#{filename}")
+  
+  # File.delete('up/song.mp3')
 end
