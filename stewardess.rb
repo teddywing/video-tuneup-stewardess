3a5c1b91af4dde0e7270d21aca356133e5f4d8d0
Brian Jordan <bcjordan@gmail.com>
2012-04-26 01:23:32 -0400

    add earworm, outline of stewardess.rb approach

--- stewardess.rb
+++ stewardess.rb
@@ -15,6 +15,12 @@ post '/blend/:filename' do
   File.open(filename, 'wb') do |file|
     file.write(datafile[:tempfile].read)
   end
+
+  # Run something like:
+  # length = params[:length] # Desired length (in seconds)
+  # [console] python src/earworm.py -d #{length} -l #{filename}
+  # send_file("*.mp3")
+  # [console] rm *.mp3
   
   send_file('up/thefile.jpg')
 end
